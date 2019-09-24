da3b5e2be510ba01a4bdd81b460908a5804f3b15
Stanislav Nikolaenko <grot_cs5@mail.ru>
2019-09-24 22:10:25 +0300

    task js-edu done

--- src/index.js
+++ src/index.js
@@ -8,7 +8,43 @@ module.exports = function getTimeForEducation(
     focus = 'family', 
     knowsProgramming = true,
     config = {family: 4}
-    ) {
-      return 0;
-  };
+    ) { 
+      let hours_week ;
+      //focus_perfomance
+      if(focus== 'top_peformance'){
+          hours_week =config['top_peformance'];
+      }
+      //focus_family
+      if(focus== 'family'){
+        hours_week =config['family'];
+      }
+      //focus friends
+      if(focus== 'friends'){
+        hours_week =config['friends'];
+      }
+      //normal_life
+      if(focus== 'normal_life'){
+        hours_week =config['normal_life'];
+      }
+      //profession
+      if(focus== 'profession'){
+        hours_week =config['profession'];
+      }
+      //carrier
+      if(focus== 'carrier'){
+        hours_week =config['carrier'];
+      }
+
+
+      let all_education = 1300;
+      if(knowsProgramming){
+        all_education = 800;
+      }
+      let weeks = all_education/hours_week;
+      return Math.ceil(weeks);
+    }
+    
+     
+
+  
   
